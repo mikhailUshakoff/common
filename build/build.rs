9d96a2387281608c74dd4557f37343135aba9d86
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2023-10-16 12:32:24 +0200

    rename

--- build/build.rs
+++ build/build.rs
@@ -1,5 +1,5 @@
 /*
-* Copyright 2018-2022 TON DEV SOLUTIONS LTD.
+* Copyright 2018-2023 EVER DEV SOLUTIONS LTD.
 *
 * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
 * this file except in compliance with the License.
@@ -7,7 +7,7 @@
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific TON DEV software governing permissions and
+* See the License for the specific EVER DEV software governing permissions and
 * limitations under the License.
 */
 
